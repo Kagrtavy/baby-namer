2111c3d23dd677f176dab9cf55e4b2d368991368
Kagrtavy <nesterivan97@gmail.com>
2024-10-18 10:57:20 +0300

    add form

---
+++ views/form.php
@@ -0,0 +1,8 @@
+<form>
+    <label>
+        Enter name of the child: <input type="text" name="name"/>
+    </label>
+    <label>
+        Enter gender of the child: <input type="text" name="gender"/>
+    </label>
+</form>
