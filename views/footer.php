a09b12513aae90af10ae8cda16b659fece426fca
IvanNSTR <164381179+Kagrtavy@users.noreply.github.com>
2024-10-18 17:57:10 +0300

    Update footer.php

--- views/footer.php
+++ views/footer.php
@@ -1,4 +1,4 @@
 <?php if (isset($_GET['created_name'])):
-    $createdName = htmlspecialchars($_GET['created_name']); ?>
+    $createdName = $_GET['created_name']; ?>
     Created name - <?= $createdName ?>, 2024
 <?php endif; ?>
